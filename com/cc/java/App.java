313d9bdcb50b3216ed66fefb7ef72fbf70c82508
benni-bit <benjaminotto.1@web.de>
2025-01-29 09:08:22 +0100

    Laufzeitfehlerbehandlung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,12 +7,14 @@ public class App {
 		String str;  // String
 		char ch; // Character
 		
+		//   Wir wollen nicht das ganze System crashen;))   Bevo rder Fehler auftritt>>>IndexOutOfBounce wird dieser Fehler durch das "catch" weggefangen
+		//   Am gebräuchlisten gegen Fehler die während der Laufzeit auftreten können(Laufzeitfehlerbehandlung)
 		try 
 		{		
-			str = "Irgend eine Zeichenkette";
+			str = "HI";
 			System.out.println("Stringlaenge :" + str.length());
 			
-			ch = str.charAt(0);
+			ch = str.charAt(20);
 			System.out.println("Character :" + ch);	
 		} 
 		catch (StringIndexOutOfBoundsException e) {
